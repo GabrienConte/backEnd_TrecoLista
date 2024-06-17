f2d587ca11794b74abc808dd85d7e57e98b28895
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 14:31:59 -0300

    cria coneção aoo banco de dados

---
+++ BackEnd_TrecoLista/Infraestrutura/ConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using BackEnd_TrecoLista.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_TrecoLista.Infraestrutura
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(
+            "Server=localhost;" +
+            "Port=5432;Database=db_trecolista;" +
+            "User Id=postgres;" +
+            "Password=1234;");
+    }
+}
