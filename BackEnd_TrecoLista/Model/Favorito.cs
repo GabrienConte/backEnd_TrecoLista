b660e030be531e97e273fca7b032d522a4600b01
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 13:44:58 -0300

    feat: cria model Favorito

---
+++ BackEnd_TrecoLista/Model/Favorito.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd_TrecoLista.Model
+{
+    [Table("favorito")]
+    public class Favorito
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProdutoId { get; set; }
+
+        [Required]
+        public int UsuarioId { get; set; }
+
+        public bool Aviso { get; set; }
+
+        public int Prioridade { get; set; }
+
+        [ForeignKey("ProdutoId")]
+        public virtual Produto Produto { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public virtual Usuario Usuario { get; set; }
+
+        public Favorito(int id, int produtoId, int usuarioId, bool aviso, int prioridade, Produto produto, Usuario usuario)
+        {
+            Id = id;
+            ProdutoId = produtoId;
+            UsuarioId = usuarioId;
+            Aviso = aviso;
+            Prioridade = prioridade;
+            Produto = produto;
+            Usuario = usuario;
+        }
+    }
+}
