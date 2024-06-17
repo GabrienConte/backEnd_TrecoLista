7082df43bcf58d562922045858dadabcb695dba3
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 14:31:36 -0300

    adiciona viewmodel de usuario

---
+++ BackEnd_TrecoLista/ViewModel/UsuarioViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BackEnd_TrecoLista.ViewModel
+{
+    public class UsuarioViewModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Login { get; set; }
+        public string TipoUsuario { get; set; } = "cliente";
+    }
+}
