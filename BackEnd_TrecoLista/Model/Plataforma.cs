81002151bf9070403d613ab22b28e1e268049f77
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 13:45:15 -0300

    feat: cria model Plataforma

---
+++ BackEnd_TrecoLista/Model/Plataforma.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd_TrecoLista.Model
+{
+    [Table("plataforma")]
+    public class Plataforma
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Descricao { get; set; }
+
+        public Plataforma(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+    }
+}
