53552ddd73b8647c783dc0e06d548fc151ba9602
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 13:24:17 -0300

    Add project files.

---
+++ BackEnd_TrecoLista/Model/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd_TrecoLista.Model
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Senha { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TipoUsuario { get; set; }
+    }
+}
