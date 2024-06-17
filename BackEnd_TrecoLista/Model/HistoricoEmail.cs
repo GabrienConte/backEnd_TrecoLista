724973d3af430845e419577073448b1ed33327d6
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 13:45:07 -0300

    feat: cria model Historico Email

---
+++ BackEnd_TrecoLista/Model/HistoricoEmail.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd_TrecoLista.Model
+{
+    [Table("historicoemail")]
+    public class HistoricoEmail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Assunto { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string CorpoEmail { get; set; }
+
+        [Required]
+        public DateTime DataEnvio { get; set; }
+
+        [Required]
+        public int DestinoId { get; set; }
+
+        [Required]
+        public int ProdutoId { get; set; }
+
+        [ForeignKey("DestinoId")]
+        public virtual Usuario Destino { get; set; }
+
+        [ForeignKey("ProdutoId")]
+        public virtual Produto Produto { get; set; }
+
+        public HistoricoEmail(int id, string assunto, string corpoEmail, DateTime dataEnvio, int destinoId, int produtoId, Usuario destino, Produto produto)
+        {
+            Id = id;
+            Assunto = assunto;
+            CorpoEmail = corpoEmail;
+            DataEnvio = dataEnvio;
+            DestinoId = destinoId;
+            ProdutoId = produtoId;
+            Destino = destino;
+            Produto = produto;
+        }
+    }
+}
