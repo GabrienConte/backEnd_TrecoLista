d9c8a19450c151216025975082285dd67d22fa24
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 13:28:56 -0300

    feat: cria model de categoria

---
+++ BackEnd_TrecoLista/Model/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd_TrecoLista.Model
+{
+    [Table("categoria")]
+    public class Categoria
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
