bffa1f3304cf8d2b957e72784c7ea6a237a1f43b
GabrienConte <gabrielconte0@gmail.com>
2024-06-17 14:31:45 -0300

    adiciona controller de usuario

---
+++ BackEnd_TrecoLista/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using BackEnd_TrecoLista.Model;
+using BackEnd_TrecoLista.Repository.Interfaces;
+using BackEnd_TrecoLista.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_TrecoLista.Controllers
+{
+    [ApiController]
+    [Route("api/v1/usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository ?? throw new ArgumentNullException();
+        }
+
+        [HttpPost]
+        public IActionResult Add(UsuarioViewModel usuarioViewModel)
+        {
+            var usuario = new Usuario(
+                0,
+                usuarioViewModel.Email,
+                usuarioViewModel.Senha,
+                usuarioViewModel.Login,
+                usuarioViewModel.TipoUsuario
+                );
+            _usuarioRepository.Add(usuario);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var usuarios = _usuarioRepository.Get();
+
+            return Ok(usuarios);
+        }
+    }
+}
